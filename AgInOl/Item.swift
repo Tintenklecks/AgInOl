624e5598a70af99d90343524323ec2df0fe9be7c
Puco <puco@me.com>
2026-07-18 19:34:51 +0200

    Initial Commit

---
+++ AgInOl/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AgInOl
+//
+//  Created by puco on 18.07.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AgInOlTests/AgInOlTests.swift
@@ -0,0 +1,19 @@
+//
+//  AgInOlTests.swift
+//  AgInOlTests
+//
+//  Created by puco on 18.07.2026.
+//
+
+import Testing
+@testable import AgInOl
+
+struct AgInOlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
